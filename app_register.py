76d1336af2b14fa2fa2b6f2b27fdceeed5233aea
willow_92 <david383@naver.com>
2023-02-06 01:37:36 +0900

    2023.02.06 list.html -> 신조어 등록 api 완성 (temp_word_register.html, app_register.py 참고) 익일 DB에 샘플로 데이터 더 넣어 놓기 (송경)

--- app_register.py
+++ app_register.py
@@ -9,24 +9,17 @@ db = client.dbsparta
 
 @app.route('/')
 def main():
-    return render_template('temp_word_regist.html')
+    return render_template('temp_word_register.html')
 
-@app.route('/home')
-def home():
-    return render_template('temp_word_regist.html')
-
-@app.route('/list')
-def list():
-    return render_template('temp_word_regist.html')
 
 @app.route("/word", methods=["POST"])
-def reply_post():
+def desc_post():
 
     word_receive = request.form['word_give']
     name_receive = request.form['name_give']
     img_url_receive = request.form['img_url_give']
     youtube_url_receive = request.form['youtube_url_give']
-    desc_url_receive = request.form['desc_url_give']
+    desc_url_receive = request.form['desc_give']
     created_at_receive = request.form['created_at_give']
     age_tag_receive = request.form['age_tag_give']
 
@@ -37,17 +30,12 @@ def reply_post():
         'youtube_url': youtube_url_receive,
         'desc_url': desc_url_receive,
         'created_at': created_at_receive,
-        'age_tag': age_tag_receive,
+        'age_tag': age_tag_receive
     }
 
-    # db.register.insert_one(doc)
-    print(doc)
-    # return jsonify({'msg': '작성 완료!'})
+    db.register.insert_one(doc)
+    return jsonify({'msg': '작성 완료!'})
 
-# @app.route("/detail", methods=["GET"])
-# def reply_get():
-#     reply_list = list(db.replys.find({}, {'_id': False}))
-#     return jsonify({'replys': reply_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
