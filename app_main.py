ab9b46f001e9f0278aefc4fb896360ad642c7996
mingcasey <socialmingj@gmail.com>
2023-02-08 03:41:23 +0900

    민정: 신조어 등록 건수 노출, 신조어 리스트 노출 기능 추가

--- app_main.py
+++ app_main.py
@@ -9,17 +9,26 @@ db = client.dbsparta
 
 @app.route('/')
 def home():
-   return render_template('')
-
+   return render_template('index.html')
 
 @app.route("/word", methods=["GET"])
 def word_get():
-    word_list = list(db.register.find({'name':'날슝이'},{'_id':False}))
-    count = db.register.count_documents({})
-    print(count)
-    return jsonify({'word':word_list})
+    word_list = list(db.register.find({},{'_id':False}))
+    return jsonify({'words':word_list})
 
+
+@app.route("/word", methods=["GET"])
+def time_get():
+    time_list = list(db.register.find({'created_at'}))
+    return jsonify({'times':time_list})
+
+
+
+@app.route("/counting", methods=["GET"])
+def count_get():
+    db_count = db.register.count_documents({})
+    return jsonify({'counts': db_count})
 
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=9999, debug=True)
+   app.run('0.0.0.0', port=5000, debug=True)
